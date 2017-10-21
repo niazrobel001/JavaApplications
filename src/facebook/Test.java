fa8d047d86dc6208bdaac519bf8550c8ffb1d5c5
Niaz <Niaz@192.168.0.4>
2017-10-21 00:55:08 -0400

    First Commit

---
+++ src/facebook/Test.java
@@ -0,0 +1,27 @@
+package facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+	//WebDriver driver =new ChromeDriver();
+	
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Niaz\\Downloads\\chrome\\chromedriver.exe");
+	WebDriver driver =new ChromeDriver();
+	
+		/*System.setProperty("webdriver.gecko.driver","C:\\Users\\Niaz\\Documents\\firefox\\geckodriver.exe");
+		//System.setProperty("webdriver.firefox.marionette","C:\\\\Users\\\\Niaz\\\\Documents\\\\firefox\\\\geckodriver.exe");
+	WebDriver driver =new FirefoxDriver();*/
+	
+
+	driver.get("https://www.facebook.com/");
+	}
+
+	
+	}
+	
+
